994098a3272e30944ba71b6c39c46bed625be1d1
Isaev Igor <igor.igorovich.isaev@gmail.com>
2021-09-10 15:48:39 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import './styles/App.css'
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'JavaScript 1', body: 'description'},
+        {id: 2, title: 'JavaScript 2', body: 'description'},
+        {id: 3, title: 'JavaScript 3', body: 'description'}
+    ])
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    };
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+    return (
+        <div className={'App'}>
+            <PostForm create={createPost}/>
+            <hr style={{margin:'15px 0'}}/>
+            <div>
+                <select>
+                    <option value="1111">By title</option>
+                    <option value="1111">By description</option>
+                </select>
+            </div>
+            {posts.length
+                ?<PostList remove={removePost} posts={posts} title={'Post by JS'}/>
+                :<h1 style={{textAlign:'center'}}>Post not found</h1>
+            }
+
+        </div>
+    )
+        ;
 }
 
 export default App;
